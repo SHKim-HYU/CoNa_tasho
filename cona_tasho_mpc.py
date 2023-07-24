6e228e5acee086ab1c3f799036db60c8046a6727
SHKim-HYU <tjsghd101@naver.com>
2023-07-24 19:41:11 +0900

    no simulation

--- cona_tasho_mpc.py
+++ cona_tasho_mpc.py
@@ -26,7 +26,7 @@ from numpy import pi, cos, sin, tan, square
 from casadi import vertcat, horzcat, sumsqr, arctan2
 from casadi import pi, cos, sin
 from rockit import MultipleShooting, Ocp
-# from waypoints_functions import find_closest_point, find_closest_obstacle, index_last_point_fun, get_current_waypoints
+from waypoints_functions import find_closest_point, find_closest_obstacle, index_last_point_fun, get_current_waypoints
 
 import casadi as cs
 import matplotlib.pyplot as plt
@@ -64,61 +64,6 @@ horizon_samples = 50
 t_mpc = 0.1 #in seconds
 # horizon_samples = 75
 # t_mpc = 1/15
-
-################################################
-# Define some functions to match current position with reference path
-################################################
-
-# Find closest point on the reference path compared witch current position
-def find_closest_point(pose, reference_path, start_index):
-    # x and y distance from current position (pose) to every point in 
-    # the reference path starting at a certain starting index
-    xlist = reference_path['x'][start_index:] - pose[0]
-    ylist = reference_path['y'][start_index:] - pose[1]
-    # Index of closest point by Pythagoras theorem
-    index_closest = start_index+np.argmin(np.sqrt(xlist*xlist + ylist*ylist))
-    print('find_closest_point results in', index_closest)
-    return index_closest
-
-def find_closest_obstacle(pose,reference_obs):
-    xlist = reference_obs['x'][:] - pose[0]
-    ylist = reference_obs['y'][:] - pose[1]
-    dist_closest = np.min(np.sqrt(xlist*xlist + ylist*ylist))
-    index_closest = np.argmin(np.sqrt(xlist*xlist + ylist*ylist))
-    return dist_closest,index_closest
-
-# Return the point on the reference path that is located at a certain distance 
-# from the current position
-def index_last_point_fun(start_index, wp, dist):
-    pathpoints = wp.shape[1]
-    # Cumulative distance covered
-    cum_dist = 0
-    # Start looping the index from start_index to end
-    for i in range(start_index, pathpoints-1):
-        # Update comulative distance covered
-        cum_dist += np.linalg.norm(wp[:,i] - wp[:,i+1])
-        # Are we there yet?
-        if cum_dist >= dist:
-            return i + 1
-    # Desired distance was never covered, -1 for zero-based index
-    return pathpoints - 1
-
-# Create a list of N waypoints
-def get_current_waypoints(start_index, wp, N, dist):
-    # Determine index at reference path that is dist away from starting point
-    last_index = index_last_point_fun(start_index, wp, dist)
-    # Calculate amount of indices between last and start point
-    w_index = last_index - start_index
-    # Dependent on the amount of indices, do
-    if w_index >= N: 
-        # There are more than N path points available, so take the first N ones
-        index_list = list(range(start_index, start_index+N+1))
-        print('index list with >= N points:', index_list)
-    else:
-        # There are less than N path points available, so add the final one multiple times
-        index_list = list(range(start_index, last_index)) + [last_index]*(N-w_index+1)
-        print('index list with < N points:', index_list)
-    return wp[:,index_list]
 
 #############################################################################
 ################## Manager for global data multiprocessing ##################
