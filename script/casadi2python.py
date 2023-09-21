1047963d18d150a1b4517735250d1dd2ac6ef685
SHKim-HYU <tjsghd101@naver.com>
2023-09-21 23:38:29 +0900

    rospkg

--- script/casadi2python.py
+++ script/casadi2python.py
@@ -16,27 +16,33 @@ import casadi as cs
 
 i0=[0.0]*814 # Serialized inputs: please refer to .json file
 
-### 1. OCP: Initial guess(first loop)
-tc_ocp = cs.Function.load('../lib/casadi/tc_ocp.casadi')
-ocp_res = tc_ocp(i0)
-print("OCP Result: ", ocp_res)
+# ### 1. OCP: Initial guess(first loop)
+# print("OCP Input Size: ", len(i0))
+# tc_ocp = cs.Function.load('../lib/casadi/tc_ocp.casadi')
+# print(tc_ocp)
+# ocp_res = tc_ocp(i0)
+# print("OCP Output Size: ", ocp_res.size())
+# print("OCP Result: ", ocp_res)
 
 ### 2. MPC: MPC function in online loop
+print("MPC Input Size: ", len(i0))
 tc_mpc=cs.Function.load('../lib/casadi/tc_mpc.casadi')
+print(tc_mpc)
 mpc_res = tc_mpc(i0)
+print("MPC Output Size: ", mpc_res.size())
 print("MPC Result: ", mpc_res)
 
 
-### 3. Pred: Simulate the system by one step and shift the states and control vectors to warmstart the MPC
-x0=[0.0]*9; # States input
-u=[0.0]*2;  # Control input
-T=0.0; 		# 
-t0=0.25; 	# initial time
-p=[0.0]*15  # initial parameters
-z0=[0.0]
-tc_pred=cs.Function.load('../lib/casadi/tc_pred.casadi')
+# ### 3. Pred: Simulate the system by one step and shift the states and control vectors to warmstart the MPC
+# x0=[0.0]*9; # States input
+# u=[0.0]*2;  # Control input
+# T=0.0; 		# 
+# t0=0.25; 	# initial time
+# p=[0.0]*15  # initial parameters
+# z0=[0.0]
+# tc_pred=cs.Function.load('../lib/casadi/tc_pred.casadi')
 
-# xf: , Xi: , poly_coeff: Coefficient matrix from RK4 to reconstruct 4th order polynomial (k1,k2,k3,k4),
-# qf: , zf: , Zi: , poly_coeff_z: 
-xf, Xi, poly_coeff, qf, zf, Zi, poly_coeff_z = tc_pred(x0,u,T,t0,p,z0)
-print("Pred Result: ", xf, Xi, poly_coeff, qf, zf, Zi, poly_coeff_z)
+# # xf: , Xi: , poly_coeff: Coefficient matrix from RK4 to reconstruct 4th order polynomial (k1,k2,k3,k4),
+# # qf: , zf: , Zi: , poly_coeff_z: 
+# xf, Xi, poly_coeff, qf, zf, Zi, poly_coeff_z = tc_pred(x0,u,T,t0,p,z0)
+# print("Pred Result: ", xf, Xi, poly_coeff, qf, zf, Zi, poly_coeff_z)
--- src/casadi2cpp.cpp
+++ src/casadi2cpp.cpp
@@ -14,82 +14,89 @@
 
 int main() {
 
-    //// 1. OCP: Initial guess
-    // CasADi Function Load.
-    casadi::Function tc_ocp = casadi::Function::load("../lib/casadi/tc_ocp.casadi");
+    // //// 1. OCP: Initial guess
+    // // CasADi Function Load.
+    // casadi::Function tc_ocp = casadi::Function::load("../lib/casadi/tc_ocp.casadi");
+    // std::cout << "tc_ocp: " << tc_ocp << std::endl;
 
-    // Input Setting..
-    casadi::DM ocp_i0 = casadi::DM::zeros(814);
+    // // Input Setting..
+    // casadi::DM ocp_i0 = casadi::DM::zeros(814);
+    // std::cout << "MPC Input Size: " << ocp_i0.size() << std::endl;
 
-    // Evaluate CasADi Function
-    std::vector<casadi::DM> ocp_arg = {ocp_i0};
-    std::vector<casadi::DM> ocp_res = tc_ocp(ocp_arg);
+    // // Evaluate CasADi Function
+    // std::vector<casadi::DM> ocp_arg = {ocp_i0};
+    // std::vector<casadi::DM> ocp_res = tc_ocp(ocp_arg);
 
-    // results.
-    casadi::DM ocp_o0 = ocp_res[0];
+    // // results.
+    // casadi::DM ocp_o0 = ocp_res[0];
+    // std::cout << "OCP Output Size: " << ocp_o0.size() << std::endl;
 
-    // Print res
-    std::cout << "OCP Result: " << ocp_o0 << std::endl;
+    // // Print res
+    // std::cout << "OCP Result: " << ocp_o0 << std::endl;
 
 
     //// 2. MPC: MPC function in online loop
     // CasADi Function Load.
     casadi::Function tc_mpc = casadi::Function::load("../lib/casadi/tc_mpc.casadi");
+    std::cout << "tc_mpc: " << tc_mpc << std::endl;
 
     // Input Setting..
     casadi::DM mpc_i0 = casadi::DM::zeros(814);
+    std::cout << "MPC Input Size: " << mpc_i0.size() << std::endl;
 
     // Evaluate CasADi Function
     std::vector<casadi::DM> mpc_arg = {mpc_i0};
-    std::vector<casadi::DM> mpc_res = tc_ocp(mpc_arg);
+    std::vector<casadi::DM> mpc_res = tc_mpc(mpc_arg);
 
     // results.
     casadi::DM mpc_o0 = mpc_res[0];
+    std::cout << "MPC Output Size: " << mpc_o0.size() << std::endl;
 
     // Print res
     std::cout << "MPC Result: " << mpc_o0 << std::endl;
 
 
-    //// 3. Pred: Simulate the system by one step and shift the states and control vectors to warmstart the MPC
-    // CasADi Function Load.
-    casadi::Function tc_pred = casadi::Function::load("../lib/casadi/tc_pred.casadi");
+    // //// 3. Pred: Simulate the system by one step and shift the states and control vectors to warmstart the MPC
+    // // CasADi Function Load.
+    // casadi::Function tc_pred = casadi::Function::load("../lib/casadi/tc_pred.casadi");
+    // std::cout << "tc_pred: " << tc_pred << std::endl;
 
-    // Input data
-    std::vector<double> x0(9, 0.0); // States, Size of arg, initial value
-    std::vector<double> u(2, 0.0); // Control inputs, Size of arg, initial value
-    double T = 0.0;   // 
-    double t0 = 0.02; // initial_time
-    std::vector<double> p(15, 0.0); // Size of arg, initial value
-    double z0 = 0.0;
+    // // Input data
+    // std::vector<double> x0(9, 0.0); // States, Size of arg, initial value
+    // std::vector<double> u(2, 0.0); // Control inputs, Size of arg, initial value
+    // double T = 0.0;   // 
+    // double t0 = 0.02; // initial_time
+    // std::vector<double> p(15, 0.0); // Size of arg, initial value
+    // double z0 = 0.0;
 
-    // Prepare input as DM objects
-    casadi::DM dm_x0 = casadi::DM(x0);
-    casadi::DM dm_u = casadi::DM(u);
-    casadi::DM dm_T = casadi::DM(T);
-    casadi::DM dm_t0 = casadi::DM(t0);
-    casadi::DM dm_p = casadi::DM(p);
-    casadi::DM dm_z0 = casadi::DM(z0);
+    // // Prepare input as DM objects
+    // casadi::DM dm_x0 = casadi::DM(x0);
+    // casadi::DM dm_u = casadi::DM(u);
+    // casadi::DM dm_T = casadi::DM(T);
+    // casadi::DM dm_t0 = casadi::DM(t0);
+    // casadi::DM dm_p = casadi::DM(p);
+    // casadi::DM dm_z0 = casadi::DM(z0);
 
-    // Prepare the input dictionary
-    std::map<std::string, casadi::DM> arg;
-    arg["x0"] = dm_x0;
-    arg["u"] = dm_u;
-    arg["T"] = dm_T;
-    arg["t0"] = dm_t0;
-    arg["p"] = dm_p;
-    arg["z0"] = dm_z0;
+    // // Prepare the input dictionary
+    // std::map<std::string, casadi::DM> arg;
+    // arg["x0"] = dm_x0;
+    // arg["u"] = dm_u;
+    // arg["T"] = dm_T;
+    // arg["t0"] = dm_t0;
+    // arg["p"] = dm_p;
+    // arg["z0"] = dm_z0;
 
-    // Call the function
-    std::map<std::string, casadi::DM> res = tc_pred(arg);
+    // // Call the function
+    // std::map<std::string, casadi::DM> res = tc_pred(arg);
 
-    // Extract and use the outputs
-    casadi::DM xf = res.at("xf");
-    casadi::DM Xi = res.at("Xi");
-    casadi::DM poly_coeff = res.at("poly_coeff");
+    // // Extract and use the outputs
+    // casadi::DM xf = res.at("xf");
+    // casadi::DM Xi = res.at("Xi");
+    // casadi::DM poly_coeff = res.at("poly_coeff");
 
-    std::cout << "xf: " << xf << std::endl;
-    std::cout << "Xi: " << Xi << std::endl;
-    std::cout << "poly_coeff: " << poly_coeff << std::endl;
+    // std::cout << "xf: " << xf << std::endl;
+    // std::cout << "Xi: " << Xi << std::endl;
+    // std::cout << "poly_coeff: " << poly_coeff << std::endl;
 
     return 0;
 }
